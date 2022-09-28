9a07c25097915c48c0c1227459a7573950bed804
Devisriram <20pa1a05h4@vishnu.edu.in>
2022-09-29 00:14:06 +0530

    Added express to application

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hi");
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get("/", (req, res) => {
+    res.send('Hellow World!.. how an i');
+ });
+
+app.get("/helo", (req, res) => {
+    res.send('Hellow World!.. how an i');
+ }); 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+ });
