156e787dc0581eb90f376c60d0767cb29590449e
Devisriram <20pa1a05h4@vishnu.edu.in>
2022-10-10 22:01:20 +0530

    final

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const request = require('request');
 
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
@@ -16,7 +17,7 @@ const db = getFirestore();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send('Hellow World!.. how an i');
+    res.render('start.ejs');
  });
 
 app.get("/signup", (req, res) => {
@@ -25,22 +26,27 @@ app.get("/signup", (req, res) => {
  
 app.get('/signupsubmit', (req, res) => {
     
-    const full_name = req.query.full_name;
+    const full_name = req.query.first_name;
     const last_name = req.query.last_name;
-    const email = req.query.emil;
-    const password = req.query.passwrd;
+    const email = req.query.email;
+    const password = req.query.psw;
+    const rep_psw = req.query.psw_repeat;
+
     
     //Adding data to the collection
-    // sample comment
-    db.collection('users')
-    .add({
-        name: full_name + last_name,
-        email:emil,
-        password: passwrd,
-    })
-    .then(() => {
-        res.send("SignUP successfully");
-    });
+    if(password == rep_psw){
+        db.collection('users')
+        .add({
+            name: full_name + last_name,
+            email:email,
+            password: password,
+        })
+        .then(() => {
+            res.render("signin");
+        });
+    }else{
+        res.send("SignUP Failed");
+    }
 });
 
 app.get("/signin", (req, res) => {
@@ -57,7 +63,6 @@ app.get("/signin", (req, res) => {
         .get()
         .then((docs) => {
             if(docs.size>0){
-                //query my data base with all the user data only when the login is successful
                 var usersData = [];
                 db.collection('users')
                     .get()
@@ -68,14 +73,114 @@ app.get("/signin", (req, res) => {
                 })
                 .then(() => {
                     console.log(usersData);
-                    res.render('home', {userData: usersData});
+                    res.render('home' , {userData: usersData},);
                 }); 
             }else{
-                res.send("login failed");
+                res.send("Login Failed");
             }
         });
  });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+ app.get("/end", (req, res) => {
+    res.render('end');
  });
+
+ app.get("/movie", (req, res) => {
+    res.render('movie');
+ });
+
+const API_KEY = 'api_key=65c72562ffa13a6ef5e5967eb6186747';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY; 
+
+
+ app.get("/movienamesearch", (req, res) => {
+    const moviename = req.query.moviename;
+    var movieData = []
+
+//    getMovies(API_URL);
+    var url;
+    if(moviename){
+        url = searchURL+'&query='+moviename;
+    }
+    else{
+        url = API_URL;
+    }
+    request(url , function(error,response,body){
+        var data = JSON.parse(body).results;
+        if(data){
+            //console.log(JSON.parse(body).results);
+            showMovies(data);
+            
+            function showMovies(data) { 
+                data.forEach(movie => {
+                    movieData.push(movie);
+                })
+            }
+            console.log(movieData);
+            res.render('movie', {userData: movieData},);
+            //const ejs = require('ejs');
+            //const html = await ejs.renderFile('movie', {userData: movieData}, {async: true});
+            //res.send(html);
+        }
+        else{
+            console.log("not thier");
+        }
+    })
+})
+
+   /* function getMovies(url){
+
+        fetch(url).then(ans => ans.json()).then(data => {
+            console.log(data.results)
+            showMovies(data.results);
+        })
+    }
+    function showMovies(data) { 
+        data.forEach(movie => {
+            var innerdata = []
+            innerdata.push(movie.title);
+            innerdata.push(movie.poster_path);
+            innerdata.push(movie.vote_average);
+            innerdata.push(movie.overview);
+            movieData.push(movie.innerdata);
+        })
+    }
+    console.log(moviename);
+
+    if(moviename){
+        getMovies(searchURL+'&query='+moviename);
+    }else{
+        getMovies(API_URL);
+    }
+    request()
+ })*/
+ /*app.get("/movienamesearch",(req,res)=>{
+    const moviename= req.query.moviename;
+    request("https://www.omdbapi.com/?t="+moviename+"&apikey=3389068b",function(error,response,body){
+        if(JSON.parse(body).Response=="True"){
+            var movieData = [];
+            movieData.push(JSON.parse(body).Director);
+            movieData.push(JSON.parse(body).Title);
+            movieData.push(JSON.parse(body).Actors);
+            movieData.push(JSON.parse(body).Ratings);
+            movieData.push(JSON.parse(body).Poster);
+            movieData.push(JSON.parse(body).Country);
+            movieData.push(JSON.parse(body).Awards);
+            movieData.push(JSON.parse(body).imdbRating);
+            movieData.push(JSON.parse(body).Plot);
+            console.log(movieData)
+            res.render('movie', {userData: movieData},);
+            }
+        else{
+            res.send("movienameincorrect");
+        }
+    })
+})*/
+
+app.listen(port, () => {
+    console.log(`Your APP is running in the port ${port}`);
+})
